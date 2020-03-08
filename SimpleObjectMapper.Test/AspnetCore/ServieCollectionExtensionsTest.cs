76df94fe19b11884fa626f41a4ea390746fa9580
Babu <babula38@gmail.com>
2020-03-08 17:36:21 +0530

    Refactor : Rename the folder
    The folder name was different what the project name. So made the same consisistence.

--- SimpleObjectMapper.Test/AspnetCore/ServieCollectionExtensionsTest.cs
+++ SimpleObjectMapper.Test/AspnetCore/ServieCollectionExtensionsTest.cs
@@ -37,6 +37,34 @@ namespace SimpleObjectMapper.Test.AspnetCore
             services.AddTransient(typeof(IGeneric<SampleParam2>), typeof(Class2));
             //services.AddSingleton<IGeneric<SampleParam2>>(x => x.GetRequiredService<Class2>());
 
+            var provider = services.BuildServiceProvider();
+
+
+            var class1 = provider.GetService<IGeneric<SampleParam1>>(); // An instance of Foo
+            var class2 = provider.GetService<IGeneric<SampleParam2>>(); // An instance of Foo
+
+            var class2Transit = provider.GetService<IGeneric<SampleParam2>>(); // An instance of Foo
+
+            class1.Should().BeSameAs(class1Instance);
+            class2.Should().BeSameAs(class2SingleInstance);
+
+            class2.Should().BeSameAs(class2SingleInstance);
+        }
+
+        [Fact]
+        public void Close_genericText1()
+        {
+
+            var services = new ServiceCollection();
+
+            var class1Instance = new Class1();
+            var class2SingleInstance = new Class2();
+
+            //services.AddSingleton<IGeneric<SampleParam1>>(class1Instance);
+            //services.AddSingleton<IGeneric<SampleParam2>>(class2SingleInstance);
+            services.AddTransient(typeof(Class2).GetInterfaces()[0], typeof(Class2));
+            services.AddTransient(typeof(Class1).GetInterfaces()[0], typeof(Class1));
+            //services.AddSingleton<IGeneric<SampleParam2>>(x => x.GetRequiredService<Class2>());
 
             var provider = services.BuildServiceProvider();
 
